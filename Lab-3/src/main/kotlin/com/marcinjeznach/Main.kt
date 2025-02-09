d3be13f7659f590981462d596ff3cbb8a29d3ad9
Marcin Jeznach <scheissig@gmail.com>
2025-02-10 00:47:48 +0100

    add entry for Lab-3

--- Lab-3/src/main/kotlin/com/marcinjeznach/Main.kt
+++ Lab-3/src/main/kotlin/com/marcinjeznach/Main.kt
@@ -1,22 +1,18 @@
 package com.marcinjeznach
 
-import com.marcinjeznach.udp.listen
-import com.marcinjeznach.udp.multicast
-import com.marcinjeznach.utils.ETX
-import com.marcinjeznach.utils.launchIO
-import com.marcinjeznach.utils.printlnErr
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.runBlocking
-import java.net.InetAddress
 import kotlin.system.exitProcess
+import com.marcinjeznach.lab3.listener.main as main_listener
+import com.marcinjeznach.lab3.sender.main as main_sender
 
 /**
  * Main entry point for JAR generated with `mvn package`.
  */
 fun main(args: Array<String>): Unit = runBlocking {
-	when (args.getOrNull(0)
-		?.toIntOrNull()) {
+	when (args.getOrNull(0)) {
+
+		"listener" -> main_listener(args)
+		"sender" -> main_sender(args)
 
 		else -> {
 			System.err.println("Please specify program type in program args (listener / sender)!")
@@ -24,41 +20,4 @@ fun main(args: Array<String>): Unit = runBlocking {
 		}
 	}
 }
-//	System.setProperty("java.net.preferIPv4Stack", "true")
-//
-//	val group = InetAddress.getByName("224.0.0.10")
-//	val port = 1234
-//
-//	val listenResult = listen(group, port)
-//	if (listenResult.isFailure) {
-//		printlnErr(listenResult)
-//	}
-//	val (msgChan, close) = listenResult.getOrThrow()
-//
-//	launchIO {
-//		msgChan.receiveAsFlow()
-//			.collect {
-//				println("[consumer]: $it")
-//			}
-//	}
-//
-//	launchIO {
-//		val letters = ('A'..'Z').toList()
-//		val counts = (1..5).toList()
-//
-//		while (isActive) {
-//			val letter = (1..counts.random()).map { letters.random() }
-//				.joinToString(separator = ETX.toString())
-//
-//			multicast(letter, group, port)
-//			println("[producer]: $letter")
-//
-//			Thread.sleep(2000)
-//		}
-//	}
-//
-//	println("Press [return] to stop...")
-//	readln()
-//	println("Exiting...")
-
 
